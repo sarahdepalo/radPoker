264efa3b5f8d9cf3c6eaeed6acdbb5e5b578a52f
Sarah dePalo <sarahdepalo99@gmail.com>
2021-11-17 16:00:29 -0500

    changed port for heroku

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,8 @@
 
 const http = require('http');
 
-const host = "127.0.0.1";
-const port = 3000;
+const host = '0.0.0.0';
+const port = process.env.PORT || 3000;
 
 const cors = require('cors');
 const morgan = require('morgan');
